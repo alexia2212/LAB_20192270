bf785bd91b9ab0b955089baed5984e6c89a13931
alexia2212 <alexia_jg@outlook.es>
2023-09-02 12:34:58 -0500

    :D

--- app/src/main/java/com/example/lab_20192270/MainActivity.java
+++ app/src/main/java/com/example/lab_20192270/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lab_20192270;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button button = findViewById(R.id.botonJugar);
+        button.setOnClickListener(view -> {
+
+
+        });
     }
 }

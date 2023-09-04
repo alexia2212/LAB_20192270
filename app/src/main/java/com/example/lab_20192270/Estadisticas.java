35cfc1cf0ffc12f7fcc6967c247f1c560f7d1555
alexia2212 <a20192270@pucp.edu.pe>
2023-09-03 23:41:26 -0500

    se acabo xfin :D

--- app/src/main/java/com/example/lab_20192270/Estadisticas.java
+++ app/src/main/java/com/example/lab_20192270/Estadisticas.java
@@ -3,7 +3,13 @@ package com.example.lab_20192270;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Estadisticas extends AppCompatActivity {
 
@@ -13,6 +19,21 @@ public class Estadisticas extends AppCompatActivity {
         setContentView(R.layout.activity_estadisticas);
         ActionBar barra = getSupportActionBar();
         barra.setTitle("TELEAHORCADO");
+        Intent intent = this.getIntent();
+        ArrayList<String> estad = intent.getStringArrayListExtra("estadisticas");
+        LinearLayout layout = findViewById(R.id.estad);
+        for (int i=0; i<estad.size();i++){
+            TextView tv = new TextView(this);
+            tv.setTextSize(15);
+            tv.setText("Juego "+(i+1)+": "+ estad.get(i));
+            layout.addView(tv);
+        }
+        Button nuevo = findViewById(R.id.botonNuevoJuego);
+        nuevo.setOnClickListener(view -> {
+            Intent intent1 = new Intent(this, TeleAhorcado.class);
+            intent.putExtra("estadisticas", estad);
+            startActivity(intent1);
+        });
 
     }
 }
--- app/src/main/java/com/example/lab_20192270/TeleAhorcado.java
+++ app/src/main/java/com/example/lab_20192270/TeleAhorcado.java
@@ -20,6 +20,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.sql.Time;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Timer;
 
@@ -35,6 +37,8 @@ public class TeleAhorcado extends AppCompatActivity {
     private ImageView[] partes;
     private int sizePartes=6;
     private int currentPart;
+    private Instant inicio;
+    private ArrayList<String> estadisticas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +46,10 @@ public class TeleAhorcado extends AppCompatActivity {
         setContentView(R.layout.activity_tele_ahorcado);
         ActionBar barra = getSupportActionBar();
         barra.setTitle("TELEAHORCADO");
-
+        Intent intent = this.getIntent();
+        if (intent.getStringArrayListExtra("estadisticas")!=null){
+            estadisticas =(ArrayList<String>) intent.getStringArrayListExtra("estadisticas");
+        }
         words=getResources().getStringArray(R.array.words);
         wordLayout=findViewById(R.id.words);
         random= new Random();
@@ -50,9 +57,46 @@ public class TeleAhorcado extends AppCompatActivity {
         partes[0] = findViewById(R.id.cabeza);
         partes[1] = findViewById(R.id.imagen_torso);
         partes[2] = findViewById(R.id.brazo_izq);
-        partes[3] = findViewById(R.id.imageView7);
+        partes[3] = findViewById(R.id.brazo_der);
         partes[4] = findViewById(R.id.pierna_der);
         partes[5] = findViewById(R.id.pierna_izq);
+
+        inicio = Instant.now();
+        Button nuevo = findViewById(R.id.BotonNuevoJuego);
+        nuevo.setOnClickListener(view -> {
+            Intent intentN = new Intent(TeleAhorcado.this, TeleAhorcado.class);
+            intentN.putExtra("estadisticas", estadisticas);
+            startActivity(intentN);
+        });
+
+        Button buttonA = findViewById(R.id.Button_A); buttonA.setOnClickListener(view -> {letterPresionar(buttonA);});
+        Button buttonB = findViewById(R.id.Button_B); buttonB.setOnClickListener(view -> {letterPresionar(buttonB);});
+        Button buttonC = findViewById(R.id.Button_C); buttonC.setOnClickListener(view -> {letterPresionar(buttonC);});
+        Button buttonD = findViewById(R.id.Button_D); buttonD.setOnClickListener(view -> {letterPresionar(buttonD);});
+        Button buttonE = findViewById(R.id.Button_E); buttonE.setOnClickListener(view -> {letterPresionar(buttonE);});
+        Button buttonF = findViewById(R.id.Button_F); buttonF.setOnClickListener(view -> {letterPresionar(buttonF);});
+        Button buttonG = findViewById(R.id.Button_G); buttonG.setOnClickListener(view -> {letterPresionar(buttonG);});
+        Button buttonH= findViewById(R.id.Button_H); buttonH.setOnClickListener(view -> {letterPresionar(buttonH);});
+        Button buttonI = findViewById(R.id.Button_I); buttonI.setOnClickListener(view -> {letterPresionar(buttonI);});
+        Button buttonJ = findViewById(R.id.Button_J); buttonJ.setOnClickListener(view -> {letterPresionar(buttonJ);});
+        Button buttonK= findViewById(R.id.Button_K); buttonK.setOnClickListener(view -> {letterPresionar(buttonK);});
+        Button buttonL= findViewById(R.id.Button_L); buttonL.setOnClickListener(view -> {letterPresionar(buttonL);});
+        Button buttonM = findViewById(R.id.Button_M); buttonM.setOnClickListener(view -> {letterPresionar(buttonM);});
+        Button buttonN = findViewById(R.id.Button_N); buttonN.setOnClickListener(view -> {letterPresionar(buttonN);});
+        Button buttonO = findViewById(R.id.Button_O); buttonO.setOnClickListener(view -> {letterPresionar(buttonO);});
+        Button buttonP = findViewById(R.id.Button_P); buttonP.setOnClickListener(view -> {letterPresionar(buttonP);});
+        Button buttonQ = findViewById(R.id.Button_Q); buttonQ.setOnClickListener(view -> {letterPresionar(buttonQ);});
+        Button buttonR = findViewById(R.id.Button_R); buttonR.setOnClickListener(view -> {letterPresionar(buttonR);});
+        Button buttonS = findViewById(R.id.Button_S); buttonS.setOnClickListener(view -> {letterPresionar(buttonS);});
+        Button buttonT = findViewById(R.id.Button_T); buttonT.setOnClickListener(view -> {letterPresionar(buttonT);});
+        Button buttonU = findViewById(R.id.Button_U); buttonU.setOnClickListener(view -> {letterPresionar(buttonU);});
+        Button buttonV = findViewById(R.id.Button_V); buttonV.setOnClickListener(view -> {letterPresionar(buttonV);});
+        Button buttonW = findViewById(R.id.Button_W); buttonW.setOnClickListener(view -> {letterPresionar(buttonW);});
+        Button buttonX = findViewById(R.id.Button_X); buttonX.setOnClickListener(view -> {letterPresionar(buttonX);});
+        Button buttonY = findViewById(R.id.Button_Y); buttonY.setOnClickListener(view -> {letterPresionar(buttonY);});
+        Button buttonZ = findViewById(R.id.Button_Z); buttonZ.setOnClickListener(view -> {letterPresionar(buttonZ);});
+
+
 
         playJuego();
 
@@ -71,7 +115,8 @@ public class TeleAhorcado extends AppCompatActivity {
                 charViews[i].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                 charViews[i].setGravity(Gravity.CENTER);
                 charViews[i].setTextColor(Color.WHITE);
-                charViews[i].setBackgroundResource(R.drawable.letter_bg);
+                charViews[i].setTextSize(20);
+                charViews[i].setBackgroundResource(R.drawable.baseline_minimize_24);
                 wordLayout.addView(charViews[i]);
             }
 
@@ -79,6 +124,7 @@ public class TeleAhorcado extends AppCompatActivity {
             numChars=currentWord.length();
             currentPart=0;
         }
+
 
     }
 
@@ -99,25 +145,21 @@ public class TeleAhorcado extends AppCompatActivity {
             }
         }
 
-
         if(correct){
             if(numCorrect==numChars){
+                    Instant fin = Instant.now();
+                    long segundos = fin.getEpochSecond() - inicio.getEpochSecond();
+
                     AlertDialog.Builder builder = new AlertDialog.Builder(this);
                     builder.setTitle("GANASTEEE!!");
-                    builder.setMessage("Tiempo de Juego: "+ "seg");
+                    builder.setMessage("Tiempo de Juego: "+ segundos +" seg");
+                    estadisticas.add("Terminó en "+segundos+" seg");
                     builder.setPositiveButton("Jugar de nuevo", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
-                            TeleAhorcado.this.playJuego();
-
-                        }
-                    });
-
-                    builder.setPositiveButton("Salir", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            TeleAhorcado.this.finish();
-
+                            Intent intent = new Intent(TeleAhorcado.this, TeleAhorcado.class);
+                            intent.putExtra("estadisticas", estadisticas);
+                            startActivity(intent);
                         }
                     });
                     
@@ -130,20 +172,14 @@ public class TeleAhorcado extends AppCompatActivity {
         }else{
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setTitle("PERDISTE :(");
-                builder.setMessage("Tiempo de Juego: "+ "seg");
+                builder.setMessage("Vuelve a intentarlo");
+                estadisticas.add("Canceló");
                 builder.setPositiveButton("Jugar de nuevo", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        TeleAhorcado.this.playJuego();
-
-                    }
-                });
-
-                builder.setPositiveButton("Salir", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        TeleAhorcado.this.finish();
-
+                        Intent intent = new Intent(TeleAhorcado.this, TeleAhorcado.class);
+                        intent.putExtra("estadisticas", estadisticas);
+                        startActivity(intent);
                     }
                 });
 
@@ -165,6 +201,7 @@ public class TeleAhorcado extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.icono_estad){
             Intent intent = new Intent(this, Estadisticas.class);
+            intent.putExtra("estadisticas", estadisticas);
             startActivity(intent);
             return true;
         }
